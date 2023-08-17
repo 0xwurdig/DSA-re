25e427b74d075a05f92996ef000cda42b4a74a90
wurdig <michwurdig@gmail.com>
2023-08-17 16:28:05 +0530

    patterns4.2

--- patterns2.cpp
+++ patterns2.cpp
@@ -72,6 +72,59 @@ int main(){
     //     cout<<endl;
     // }
 
+//    *
+//   ***
+//  *****
+// *******
+// *******
+//  *****
+//   ***
+//    *
+    
+    // for(int i=1;i<=rows;i++){
+    //     for(int j=1;j<=rows-i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1;j<=2*i-1;j++){
+    //         cout<<"*";
+    //     }
+    //     // for(int j=1;j<i;j++){
+    //     //     cout<<j<<" ";
+    //     // }
+    //     cout<<endl;
+    // }
+    // for(int i=rows;i>=1;i--){
+    //     for(int j=1;j<=rows-i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1;j<=2*i-1;j++){
+    //         cout<<"*";
+    //     }
+    //     // for(int j=1;j<i;j++){
+    //     //     cout<<j<<" ";
+    //     // }
+    //     cout<<endl;
+    // }
+//   *   *   *   *   * 
+//  * * * * * * * * * *
+// *   *   *   *   * 
+    // for(int i=1;i<=3;i++){
+    //     for(int j=1;j<=rows;j++){
+    //         if ((j+i)%4==0 || (j%4==0 && i==2))
+    //         {
+    //             cout<<"*";
+    //             /* code */
+    //         }else{
+    //             cout<<" ";
+    //         }
+            
+    //     }
+    //     // for(int j=1;j<i;j++){
+    //     //     cout<<j<<" ";
+    //     // }
+    //     cout<<endl;
+    // }
+
 
     return 0;
 }
